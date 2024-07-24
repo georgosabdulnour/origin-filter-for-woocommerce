e937f50717bbe2aa967075732778cf9256d25f57
Georgos Abdulnour <57201746+georgosabdulnour@users.noreply.github.com>
2024-07-24 15:59:00 +0200

    create plugin main file and add main data as plugin comment

---
+++ woo-origin-filter/woo-origin-filter.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Woo Origin Filter
+ * Plugin URI:  https://github.com/georgosabdulnour/woo-origin-filter
+ * Description: Adds a filter to WooCommerce orders to filter by order origin and displays total sales for the filtered origin.
+ * Version:     1.0
+ * Author:      Georgos Abdulnour
+ * Author URI: https://github.com/georgosabdulnour
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ 
