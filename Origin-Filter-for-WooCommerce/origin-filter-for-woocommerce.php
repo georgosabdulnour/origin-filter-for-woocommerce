21e3be4a8923573ee31e69818643dde3d3650412
Georgos Abdulnour <57201746+georgosabdulnour@users.noreply.github.com>
2024-07-24 19:06:23 +0200

    prettier code

--- Origin-Filter-for-WooCommerce/origin-filter-for-woocommerce.php
+++ Origin-Filter-for-WooCommerce/origin-filter-for-woocommerce.php
@@ -11,120 +11,74 @@
  */
 
 if (!defined('ABSPATH')) {
-
     exit; // Exit if accessed directly.
-
 }
 
 
 /**
-
  * Adds a dropdown to filter orders by origin on the orders page.
-
  */
 
 add_action('restrict_manage_posts', 'filter_orders_by_origin');
 
-
 function filter_orders_by_origin() {
-
     global $typenow;
 
-
     if ('shop_order' === $typenow) {
-
         $nonce = wp_create_nonce('filter_orders_by_origin_nonce'); // Create a nonce
-
         $current_origin = isset($_GET['order_origin']) ? sanitize_text_field($_GET['order_origin']) : '';
 
-
         // Output nonce as a hidden input
-
         echo '<input type="hidden" name="filter_orders_by_origin_nonce" value="' . esc_attr($nonce) . '">';
 
-
         // Use caching to avoid direct database calls
-
         $cache_key = 'origin_filter_origins';
-
         $cache_group = 'origin_filter';
-
         $origins = wp_cache_get($cache_key, $cache_group);
 
-
         if (false === $origins) {
-
             $args = array(
-
                 'post_type' => 'shop_order',
-
                 'meta_key' => '_wc_order_attribution_utm_source',
-
                 'meta_value' => '',
-
                 'meta_compare' => '!=',
-
                 'meta_type' => 'CHAR'
-
             );
 
-
             $query = new WP_Query($args);
-
             $orders = $query->posts;
-
             $origins = array();
 
-
             foreach ($orders as $order) {
-
                 $origin = get_post_meta($order->ID, '_wc_order_attribution_utm_source', true);
-
                 $origins[] = $origin;
-
             }
-
 
             $origins = array_unique($origins);
-
             wp_cache_set($cache_key, $origins, $cache_group, 12 * HOUR_IN_SECONDS);
-
         }
 
-
-        ?>
+?>
 
         <select name="order_origin" id="order_origin">
-
             <option value=""><?php esc_html_e('All Origins', 'woocommerce'); ?></option>
-
             <?php
-
             foreach ($origins as $origin) {
-
                 $selected_attr = selected($current_origin, $origin, false);
-
                 echo '<option value="' . esc_attr($origin) . '" ' . esc_attr($selected_attr) . '>' . esc_html($origin) . '</option>';
-
             }
-
             ?>
 
         </select>
-
         <?php
-
     }
-
 }
 
 
 // Define a function to get order total from cache or database
-
 function get_order_total($order_id) {
 
     $order_total = get_post_meta($order_id, '_order_total', true);
-
     return $order_total;
 
 }
