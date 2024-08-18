517084d342acc7701b9252fb7a899d47c519d29c
Georgos Abdulnour <57201746+georgosabdulnour@users.noreply.github.com>
2024-08-18 23:39:57 +0200

    update plugin after wordpress review

--- Origin-Filter-for-WooCommerce/origin-filter-for-woocommerce.php
+++ Origin-Filter-for-WooCommerce/origin-filter-for-woocommerce.php
@@ -4,6 +4,7 @@
  * Plugin URI:  https://github.com/georgosabdulnour/origin-filter-for-woocommerce
  * Description: Adds a filter to WooCommerce orders to filter by order origin and displays total sales for the filtered origin and month.
  * Version:     1.0
+ * Text Domain: origin-filter-for-woocommerce
  * Author:      Georgos Abdulnour
  * Author URI: https://github.com/georgosabdulnour
  * License:     GPL2
@@ -17,53 +18,53 @@ if (!defined('ABSPATH')) {
 /**
  * Adds a dropdown to filter orders by origin on the orders page.
  */
-add_action('restrict_manage_posts', 'filter_orders_by_origin');
+add_action('restrict_manage_posts', 'ofwc_filter_orders_by_origin');
 
-function filter_orders_by_origin()
+function ofwc_filter_orders_by_origin()
 {
     global $typenow;
 
     if ('shop_order' === $typenow) {
-        $nonce = wp_create_nonce('filter_orders_by_origin_nonce');
-        $current_origin = isset($_GET['order_origin']) ? sanitize_text_field($_GET['order_origin']) : '';
+        $nonce = wp_create_nonce('ofwc_filter_orders_by_origin_nonce');
+        $current_origin = '';
+        if (isset($_GET['order_origin']) && isset($_GET['ofwc_nonce']) && wp_verify_nonce(sanitize_text_field(wp_unslash($_GET['ofwc_nonce'])), 'ofwc_nonce_action')) {
+            $current_origin = sanitize_text_field($_GET['order_origin']);
+        }
 
         echo '<input type="hidden" name="filter_orders_by_origin_nonce" value="' . esc_attr($nonce) . '">';
 
         // Retrieve origins from the database with caching
-        $cache_key = 'origin_filter_origins';
-        $cache_group = 'origin_filter';
+        $cache_key = 'ofwc_filter_origins';
+        $cache_group = 'ofwc_origin_filter';
         $origins = wp_cache_get($cache_key, $cache_group);
 
         if (false === $origins) {
             global $wpdb;
             
-            // Prepare the query with proper placeholders
-            $query = "
-                SELECT DISTINCT pm.meta_value as origin
-                FROM {$wpdb->prefix}postmeta pm
-                INNER JOIN {$wpdb->prefix}posts p ON p.ID = pm.post_id
-                WHERE pm.meta_key = %s
-                AND p.post_type = %s
-                AND pm.meta_value != ''
-            ";
-            
-            // Execute the query
-            $origins = $wpdb->get_col($wpdb->prepare($query, '_wc_order_attribution_utm_source', 'shop_order'));
+            $origins = $wpdb->get_col(
+                $wpdb->prepare(
+                    "
+                    SELECT DISTINCT pm.meta_value as origin
+                    FROM %1$s.postmeta pm
+                    INNER JOIN %1$s.posts p ON p.ID = pm.post_id
+                    WHERE pm.meta_key = %2$s
+                    AND p.post_type = %3$s
+                    AND pm.meta_value != ''
+                    ",
+                    $wpdb->prefix,
+                    '_wc_order_attribution_utm_source',
+                    'shop_order'
+                )
+            );
         
             // Cache the results
             wp_cache_set($cache_key, $origins, $cache_group, 12 * HOUR_IN_SECONDS);
         }
-        
-        // At this point, $origins should contain the dropdown values
-        
-        
-        
-        
 
         // Display the origin filter dropdown
         ?>
         <select name="order_origin" id="order_origin">
-            <option value=""><?php esc_html_e('All Origins', 'woocommerce');?></option>
+            <option value=""><?php esc_html_e('All Origins', 'origin-filter-for-woocommerce'); ?></option>
             <?php
             foreach ($origins as $origin) {
                 $selected_attr = selected($current_origin, $origin, false);
@@ -80,14 +81,14 @@ function filter_orders_by_origin()
  *
  * @param WP_Query $query The current query object.
  */
-add_action('pre_get_posts', 'filter_orders_by_origin_query');
+add_action('pre_get_posts', 'ofwc_filter_orders_by_origin_query');
 
-function filter_orders_by_origin_query($query)
+function ofwc_filter_orders_by_origin_query($query)
 {
     global $typenow, $pagenow;
 
     if ('shop_order' === $typenow && 'edit.php' === $pagenow && $query->is_main_query()) {
-        if (isset($_GET['order_origin']) && !empty($_GET['order_origin']) && isset($_GET['filter_orders_by_origin_nonce']) && wp_verify_nonce($_GET['filter_orders_by_origin_nonce'], 'filter_orders_by_origin_nonce')) {
+        if (isset($_GET['order_origin']) && !empty($_GET['order_origin']) && isset($_GET['filter_orders_by_origin_nonce']) && wp_verify_nonce(sanitize_text_field(wp_unslash($_GET['filter_orders_by_origin_nonce'])), 'ofwc_filter_orders_by_origin_nonce')) {
             $meta_query = array(
                 array(
                     'key' => '_wc_order_attribution_utm_source',
@@ -103,9 +104,9 @@ function filter_orders_by_origin_query($query)
 /**
  * Displays total sales for the filtered origin and month on the admin orders page.
  */
-add_action('admin_notices', 'display_total_sales_by_origin_and_month');
+add_action('admin_notices', 'ofwc_display_total_sales_by_origin_and_month');
 
-function display_total_sales_by_origin_and_month()
+function ofwc_display_total_sales_by_origin_and_month()
 {
     global $pagenow, $typenow;
 
@@ -113,7 +114,7 @@ function display_total_sales_by_origin_and_month()
         return;
     }
 
-    if (isset($_GET['order_origin']) && !empty($_GET['order_origin']) && isset($_GET['filter_orders_by_origin_nonce']) && wp_verify_nonce($_GET['filter_orders_by_origin_nonce'], 'filter_orders_by_origin_nonce')) {
+    if (isset($_GET['order_origin']) && !empty($_GET['order_origin']) && isset($_GET['filter_orders_by_origin_nonce']) && wp_verify_nonce(sanitize_text_field(wp_unslash($_GET['filter_orders_by_origin_nonce'])), 'ofwc_filter_orders_by_origin_nonce')) {
         $origin = sanitize_text_field($_GET['order_origin']);
         $order_statuses = array('wc-completed', 'wc-processing');
 
@@ -166,7 +167,7 @@ function display_total_sales_by_origin_and_month()
             <div class="notice notice-success is-dismissible">
                 <p><?php
                 // Translators: %1$s is the origin filter applied, %2$s is the total sales amount
-                $message = sprintf(__('Origin Filter applied successfully. Total of %1$s: <b>%2$s</b>', 'woocommerce'), esc_html($origin), $total_sales);
+                $message = sprintf(__('Origin Filter applied successfully. Total of %1$s: <b>%2$s</b>', 'origin-filter-for-woocommerce'), esc_html($origin), $total_sales);
                 echo wp_kses_post($message);
                 ?></p>
             </div>
@@ -176,7 +177,7 @@ function display_total_sales_by_origin_and_month()
             <div class="notice notice-error is-dismissible">
                 <p><?php
                 // Translators: %s is the origin filter applied
-                echo wp_kses_post(sprintf(__('No orders found for %s', 'woocommerce'), esc_html($origin)));
+                echo wp_kses_post(sprintf(__('No orders found for %s', 'origin-filter-for-woocommerce'), esc_html($origin)));
                 ?></p>
             </div>
             <?php
@@ -189,14 +190,18 @@ function display_total_sales_by_origin_and_month()
  *
  * @param int $order_id The order ID.
  */
-add_action('woocommerce_checkout_update_order_meta', 'save_order_origin');
+add_action('woocommerce_checkout_update_order_meta', 'ofwc_save_order_origin');
 
-function save_order_origin($order_id)
-{
-    if (isset($_POST['order_origin'])) {
-        $order_origin = sanitize_text_field($_POST['order_origin']);
-        if (!empty($order_origin)) {
-            update_post_meta($order_id, '_wc_order_attribution_utm_source', $order_origin);
+function ofwc_save_order_origin($order_id) {
+    if (
+        isset($_POST['ofwc_nonce']) &&
+        wp_verify_nonce(sanitize_text_field(wp_unslash($_POST['ofwc_nonce'])), 'ofwc_nonce_action')
+    ) {
+        if (isset($_POST['order_origin'])) {
+            $order_origin = sanitize_text_field($_POST['order_origin']);
+            if (!empty($order_origin)) {
+                update_post_meta($order_id, '_wc_order_attribution_utm_source', $order_origin);
+            }
         }
     }
 }
